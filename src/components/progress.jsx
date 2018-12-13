cb5db244697038b52c895adfc59980e9bc8db50b
T <tyson_ngo@hotmail.com>
2018-12-13 18:08:59 -0500

    implemented progress component

--- src/components/progress.jsx
+++ src/components/progress.jsx
@@ -4,16 +4,44 @@ class Progress extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			progress: 53
+			text: '',
+			progress: 0
 		};
 	}
 
 	componentDidMount(){
-		// TODO
-		// ipc on progress
-		// set state progress (out_time convert to seconds)/out_time_final * 100
-		this.props.ipc.on('progress', (e, args) => {
+		this.props.ipc.on('progress', (e, d) => {
+			if (d.progress === 'end') {
+				return this.setState({
+					text: 'video exported!',
+					progress: 100
+				})
+			}
+			let progress = (this.hhmmssToSeconds(d.out_time)/d.out_time_final * 100).toFixed(2);
+			this.setState({
+				text: `video processed: ${d.out_time} | ` +
+				`size: ${this.humanFileSize(d.total_size)} | `+
+				`bitrate: ${d.bitrate === 'N/A' ? '' : d.bitrate}`,
+				progress: progress
+			});
 		})
+	}
+
+	// https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string#answer-14919494
+	humanFileSize(bytes, si) {
+		let thresh = si ? 1000 : 1024;
+		if(Math.abs(bytes) < thresh) {
+			return bytes + ' B';
+		}
+		let units = si
+			? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+			: ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+		let u = -1;
+		do {
+			bytes /= thresh;
+			++u;
+		} while(Math.abs(bytes) >= thresh && u < units.length - 1);
+		return bytes.toFixed(1)+' '+units[u];
 	}
 
 	hhmmssToSeconds(t){
@@ -32,9 +60,9 @@ class Progress extends Component{
 			<div className='progress-container'>
 				<div className='progress-bar'>
 					<div className='progress-bar-loaded' style={{width: this.state.progress + '%'}}></div>
-					<div className='progress-bar-text'>{this.state.progress}%</div>
+					<div className='progress-bar-text'>{this.state.progress ? this.state.progress + '%' : ''}</div>
 				</div>
-				<div className='progress-text'>test</div>
+				<div className='progress-text'>{this.state.text}</div>
 			</div>
 		)
 	}
